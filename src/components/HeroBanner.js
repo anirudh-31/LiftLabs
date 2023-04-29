7549226caebc498d51d8a4393bf5985730e584af
anirudh <betrabetanirudh@gmail.com>
2023-04-29 20:40:18 +0530

    Improved home page

--- src/components/HeroBanner.js
+++ src/components/HeroBanner.js
@@ -1,18 +1,25 @@
 import React from "react";
-import { Box, Stack, Typography } from "@mui/material";
+import { Box, Button, Stack, Typography } from "@mui/material";
 import "../styles/HeroBanner.css";
+import Hero from "../assets/hero.png";
 function HeroBanner() {
   return (
     <Box
-      sx={{ mt: { lg: "212px", xs: "70px" }, ml: { sm: "50px" } }}
+      sx={{ mt: { lg: "50px", xs: "70px" }, ml: { sm: "50px" } }}
       position="relative"
       p="20px"
+      display="flex"
+      alignItems="center"
+      overflow="hidden"
     >
-      <Typography color="#FF2625" fontWeight="600" fontSize="26px">
-        Fitness Club
-      </Typography>
-      <h1>Sweat, Smile and Repeat!</h1>
-      <p className="subtitle">Check out the most effective exercises</p>
+      <div>
+        <h1>Sweat, Smile and Repeat!</h1>
+        <p className="subtitle">Check out the most effective exercises</p>
+        <Button variant="contained" color="error" href="#exercises">
+          Explore Exercises
+        </Button>
+      </div>
+      <img src={Hero} alt="" style={{ marginLeft: "-35px" }} />
     </Box>
   );
 }
